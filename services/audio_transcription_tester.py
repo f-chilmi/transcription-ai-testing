af805d242b9bdbab7d6e742fe9d0614964a0792c
f-chilmi <fchilmi5@gmail.com>
2025-06-27 21:13:41 +0700

    test

--- services/audio_transcription_tester.py
+++ services/audio_transcription_tester.py
@@ -306,7 +306,7 @@ class AudioTranscriptionTest:
         # Batch and concurrent processing
         # if len(audio_files) > 1:
         audio_list = list(audio_files.values())
-        performance_results['results']['batch_processing'] = self.test_batch_processing(audio_list, transcription_method)
+        performance_results['results']['batch_processing'] = self.test_batch_processing(audio_list, transcription_method, 'ar')
             # performance_results['results']['concurrent_processing'] = self.test_concurrent_processing(audio_list, transcription_method)
         
         end_time = time.time()
