6963e59415af15f6847de4cd851f2474baa3b7d8
f-chilmi <fchilmi5@gmail.com>
2025-06-28 00:02:57 +0700

    test

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def test_transcription_diarization() -> Dict[str, Any]:
     
     threads = 6
     model = 'tiny'
-    audio_path = 'audio_mono_swedish.mp3'
+    audio_path = 'audio_multi_swedish.mp3'
     language = 'sv'
 
     os.environ["OMP_NUM_THREADS"] = str(threads)
@@ -166,6 +166,12 @@ def test_transcription_diarization() -> Dict[str, Any]:
         gc.collect()
 
         # 2. Align whisper output
+        # alternative swedish model 
+        # swedish_models = [
+        #     "KBLab/wav2vec2-large-voxrex-swedish",
+        #     "KBLab/wav2vec2-large-xlsr-53-swedish",
+        #     "KBLab/wav2vec2-large-voxpopuli-sv-swedish"
+        # ]
         model_b, metadata = whisperx.load_align_model(language_code=language, device=device, model_name="KBLab/wav2vec2-large-voxrex-swedish")
         result = whisperx.align(result["segments"], model_b, metadata, audio, device, return_char_alignments=False)
         # self.results = result
